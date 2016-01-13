75f73371e8e58bd16e6b4b90e39e6d4c27eea895
Therese-N <tn8403@gmail.com>
2016-01-13 11:00:25 +0100

    skruffs

--- DataAccess/Repositories/TeamRepository.cs
+++ DataAccess/Repositories/TeamRepository.cs
@@ -14,7 +14,8 @@ namespace DataAccess.Repositories
         private readonly IDbConnection _dbConnection = new SqlConnection("Data Source=MAJOR\\SQLEXPRESS;Initial Catalog=Projectaa_Db;Integrated Security=True");
         public Team Add(Team team)
         {
-            const string sqlQuery = "INSERT INTO Team (Name) VALUES (@Name)";
+            const string sqlQuery = "INSERT INTO Team (Name) " +
+                                    "VALUES (@Name)";
             var teamId = _dbConnection.Query(sqlQuery, team).Single();
             team.Id = teamId;
             return team;
@@ -23,7 +24,7 @@ namespace DataAccess.Repositories
         public Team Find(int id)
         {
             return _dbConnection.Query<Team>("SELECT * FROM Team " +
-                                             "WHERE Id = @TeamId", new {id}).Single();
+                                             "WHERE Id = @TeamId", new { id }).Single();
         }
 
         public List<Team> GetAll()
@@ -33,30 +34,31 @@ namespace DataAccess.Repositories
 
         public List<User> GetUsers(int id)
         {
-            const string sqlQuery = "SELECT User_Id FROM TeamUser WHERE Team_Id = @TeamId";
-            var userIdList = _dbConnection.Query<int>(sqlQuery, new {id}).ToList();
+            const string sqlQuery = "SELECT User_Id FROM TeamUser " +
+                                    "WHERE Team_Id = @TeamId";
+            var userIdList = _dbConnection.Query<int>(sqlQuery, new { id }).ToList();
 
             return userIdList.Select(i => _dbConnection.Query<User>("SELECT * FROM User " +
-                                                                    "WHERE Id = @UserId", new {i}).Single()).ToList();
+                                                                    "WHERE Id = @UserId", new { i }).Single()).ToList();
         }
 
         public List<WorkItem> GetWorkItems(int id)
         {
             return _dbConnection.Query<WorkItem>("SELECT * FROM WorkItem " +
-                                                 "WHERE Team_Id = @TeamId", new {id}).ToList();
+                                                 "WHERE Team_Id = @TeamId", new { id }).ToList();
         }
 
         public void Remove(int id)
         {
             _dbConnection.Query("DELETE * FROM Team " +
-                                "WHERE Id = @TeamId", new {id});
+                                "WHERE Id = @TeamId", new { id });
         }
 
         public Team Update(Team team)
         {
             const string sqlQuery = "UPDATE Team " +
-                "SET Name = @Name " +
-                "WHERE Id = @Id";
+                                    "SET Name = @Name " +
+                                    "WHERE Id = @Id";
             _dbConnection.Execute(sqlQuery);
             return team;
         }
