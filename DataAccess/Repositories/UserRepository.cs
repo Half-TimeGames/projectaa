f38b3001e3feb07387bf1ffb825e6a5b3686fdc7
Therese-N <tn8403@gmail.com>
2016-01-20 14:56:07 +0100

    fyt

--- DataAccess/Repositories/UserRepository.cs
+++ DataAccess/Repositories/UserRepository.cs
@@ -10,7 +10,7 @@ namespace DataAccess.Repositories
 {
     public class UserRepository : IUserRepository
     {
-        private readonly IDbConnection _dbConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["azureConnectionString"].ConnectionString);
+        private readonly SqlConnection _dbConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["azureConnectionString"].ConnectionString);
 
         public User Add(User user)
         {
@@ -19,7 +19,7 @@ namespace DataAccess.Repositories
                            "FirstName, @LastName, @UserName)"+
                            "SELECT Id FROM [User] WHERE Id = scope_identity()";
             if (user == null) return null;
-            var userId = _dbConnection.Query(sqlQuery, new {user.FirstName, user.LastName, user.UserName}).First();
+            var userId = _dbConnection.QueryWithRetry<User>(sqlQuery, new {user.FirstName, user.LastName, user.UserName}).First();
                 
             user.Id = userId.Id;
 
@@ -28,14 +28,14 @@ namespace DataAccess.Repositories
 
         public User Find(int id)
         {
-            return _dbConnection.Query<User>("SELECT * FROM [User] " +
+            return _dbConnection.QueryWithRetry<User>("SELECT * FROM [User] " +
                                              "WHERE Id = @Id", new { id }).SingleOrDefault();
         }
 
         public List<User> FindByName(string name)
         {
             return
-                _dbConnection.Query<User>("Declare @Name varchar(100);" +
+                _dbConnection.QueryWithRetry<User>("Declare @Name varchar(100);" +
                                             "Set @Name = '%" + name + "%';" +
                                             "SET @Name = '%" + name + "%';" +
                                             "SELECT * FROM [User] " +
@@ -46,7 +46,7 @@ namespace DataAccess.Repositories
 
         public List<User> GetAll()
         {
-            return _dbConnection.Query<User>("SELECT * FROM [User]").ToList();
+            return _dbConnection.QueryWithRetry<User>("SELECT * FROM [User]").ToList();
         } 
 
         public List<User> GetAll(int page, int perPage)
@@ -61,16 +61,16 @@ namespace DataAccess.Repositories
                            "WHERE NUMBER  BETWEEN((@PageNumber - 1) * @RowspPage + 1)  AND(@PageNumber * @RowspPage)" +
                            "ORDER BY Id";
 
-            return _dbConnection.Query<User>(sqlQuery).ToList();
+            return _dbConnection.QueryWithRetry<User>(sqlQuery).ToList();
         }
 
         public List<Team> GetTeams(int id)
         {
             const string sqlQuery = "SELECT Team_Id FROM TeamUser " +
                            "WHERE User_Id = @UserId";
-            var teamIdList = _dbConnection.Query<int>(sqlQuery, new { UserId = id }).ToList();
+            var teamIdList = _dbConnection.QueryWithRetry<int>(sqlQuery, new { UserId = id }).ToList();
 
-            return teamIdList.Select(i => _dbConnection.Query<Team>("SELECT * FROM Team " +
+            return teamIdList.Select(i => _dbConnection.QueryWithRetry<Team>("SELECT * FROM Team " +
                                                                     "WHERE Id = @TeamId", new { TeamId = i }).Single()).ToList();
         }
 
@@ -78,7 +78,7 @@ namespace DataAccess.Repositories
         {
             const string sqlQuery = "DELETE FROM [User] " +
                            "WHERE Id = @Id";
-            _dbConnection.Execute(sqlQuery, new { id });
+            _dbConnection.QueryWithRetry<User>(sqlQuery, new { id });
         }
 
         public User AddTeamToUser(int userId, int teamId)
@@ -87,7 +87,7 @@ namespace DataAccess.Repositories
                                     "VALUES (@Team_Id, @User_Id) " +
                                     "SELECT * FROM [User] " +
                                     "WHERE Id = @User_Id";
-            return _dbConnection.Query<User>(sqlQuery, new { User_Id = userId, Team_Id = teamId }).First();
+            return _dbConnection.QueryWithRetry<User>(sqlQuery, new { User_Id = userId, Team_Id = teamId }).First();
         }
 
         public User Update(User user)
@@ -97,14 +97,15 @@ namespace DataAccess.Repositories
                            "FirstName = @FirstName," +
                            "LastName = @LastName," +
                            "UserName = @UserName" +
-                           " WHERE Id = @Id";
-            _dbConnection.Execute(sqlQuery, new {user.FirstName, user.LastName, user.UserName, user.Id});
-            return user;
+                           " WHERE Id = @Id " +
+                           "SELECT * FROM [User] WHERE Id = @Id";
+            var newUser = _dbConnection.QueryWithRetry<User>(sqlQuery, new {user.FirstName, user.LastName, user.UserName, user.Id}).Single();
+            return newUser;
         }
 
         public List<WorkItem> WorkItems(int id)
         {
-            return _dbConnection.Query<WorkItem>("SELECT * FROM WorkItem " +
+            return _dbConnection.QueryWithRetry<WorkItem>("SELECT * FROM WorkItem " +
                                                  "WHERE User_Id = @UserId", new { UserId = id }).ToList();
         }
     }
